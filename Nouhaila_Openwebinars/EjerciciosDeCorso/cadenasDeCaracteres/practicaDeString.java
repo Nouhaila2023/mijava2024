54071a9e6529df63eb990af35e7486902c8d9537
Nouhaila2023 <nelh237@g.educaand.es>
2024-01-07 19:39:36 +0100

    ejercico

--- Nouhaila_Openwebinars/EjerciciosDeCorso/cadenasDeCaracteres/practicaDeString.java
+++ Nouhaila_Openwebinars/EjerciciosDeCorso/cadenasDeCaracteres/practicaDeString.java
@@ -1,3 +1,11 @@
+package Nouhaila_Openwebinars.EjerciciosDeCorso.cadenasDeCaracteres;
+
 public class practicaDeString {
-    
+    public static void main(String[] args) {
+        
+        String msg = "" + 36.9;
+        System.out.println(msg);
+
+    }    
+
 }
