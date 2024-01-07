d6ab6e850f46c12ffe8026daf2a314673b695f6d
Nouhaila2023 <nelh237@g.educaand.es>
2024-01-07 20:50:04 +0100

    practica

--- Nouhaila_Openwebinars/EjerciciosDeCorso/cadenasDeCaracteres/practicaDeString.java
+++ Nouhaila_Openwebinars/EjerciciosDeCorso/cadenasDeCaracteres/practicaDeString.java
@@ -6,6 +6,30 @@ public class practicaDeString {
         String msg = "" + 36.9;
         System.out.println(msg);
 
-    }    
 
+        String msg1 = String.format("Hola, estoy bien %s ", "¿y tu?" );
+        System.out.println(msg1);
+
+        char caracter = 'a';
+        String msg2 = String.format("Alejandro Empieza con la letra %c ", 'a');
+        System.out.println(msg2);
+
+        int num = 20;
+        String msg3 = String.format("%c) \"La suma de :\n 10 + 10 = %d ",caracter ,num );
+        System.out.println(msg3);
+
+        double numero = 20.400;
+        String msg4 = String.format("La edad de Alejandro es %.3f ", numero );
+        System.out.println(msg4);
+
+        System.out.printf("Numero PI: %e\n" , Math.PI);
+        
+        char ascii = 'A';
+        int resultado = (int) ascii;
+        System.out.println(resultado);
+
+        
+
+
+    }    
 }
